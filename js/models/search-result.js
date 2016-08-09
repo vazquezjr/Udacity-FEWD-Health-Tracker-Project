5aabbd0a1e3b13fb46be540f801a4b1ccf29c085
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-08-09 13:07:37 -0400

    Add comments
    Added comments to js/models/search-results.js.

--- js/models/search-result.js
+++ js/models/search-result.js
@@ -1,8 +1,10 @@
 
 var app = app || {};
 
+// Create a model for each individual search result.
 app.Result = Backbone.Model.extend({
 
+	// Set the default attributes for the model.
 	defaults: {
 		name: '',
 		calorieCount: 0
