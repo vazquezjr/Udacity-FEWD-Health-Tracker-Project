c6dcd965302b600fc01d2fb7294218b25136a012
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-08-09 13:04:12 -0400

    Add comments
    Added comments to js/collections/results.js

--- js/collections/results.js
+++ js/collections/results.js
@@ -1,12 +1,16 @@
 
 var app = app || {};
 
+// Create a collection of search results.
 var ResultList = Backbone.Collection.extend({
 
+	// Associate a model for the collection.
 	model: app.Result,
 
+	// Create a LocalStorage object for the collection.
 	localStorage: new Backbone.LocalStorage('results-backbone')
 
 });
 
+// Create a new collection of search results.
 app.Results = new ResultList();
