79d7b1cf0bdf7d4c46cb761b2ab5fc3022bdcdcb
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-08-09 13:06:50 -0400

    Add comments
    Added comments to js/models/food-item.js.

--- js/models/food-item.js
+++ js/models/food-item.js
@@ -1,8 +1,10 @@
 
 var app = app || {};
 
+// Create a model for each individual food item.
 app.Food = Backbone.Model.extend({
 
+	// Set the default attributes for the model.
 	defaults: {
 		name: '',
 		calorieCount: 0
