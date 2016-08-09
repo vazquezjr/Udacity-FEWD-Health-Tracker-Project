d4facb5463ef49aa7ef5c412b376b53b93f35fb7
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-08-09 13:03:29 -0400

    Add comments
    Added comments to js/collections/food.js

--- js/collections/food.js
+++ js/collections/food.js
@@ -1,23 +1,31 @@
 
 var app = app || {};
 
+// Create a collection of food items.
 var FoodList = Backbone.Collection.extend({
 	
+	// Associate a model for the collection.
 	model: app.Food,
 
+	// Create a LocalStorage object for the collection.
 	localStorage: new Backbone.LocalStorage('food-backbone'),
 
+	// A function that calculates the total calories of all of the food items in this collection.
 	calculateCalories: function() {
-		//console.log(this);
+		
+		// Declare a local variable.
 		var totalCalories = 0;
 
+		// Add the calories of all of the food items to the local variable.
 		for (var i = 0; i < this.length; i++) {
 			totalCalories += this.models[i].attributes.calorieCount;
 		}
 
+		// Return the result.
 		return totalCalories;
 	}
 
 });
 
+// Create a new collection of food items.
 app.Foods = new FoodList();
