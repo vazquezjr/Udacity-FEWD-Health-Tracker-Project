2da67c94d7c5c846f9a6f1886de6111cb1acb029
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-08-09 11:56:02 -0400

    Add functions
    Added functions to add search results and food items to the view, added a function to recalculate the total calories when a food item is removed from the list, added new listeners, added a line of code to fetch the collection of food from the last page load, edited the render function to show and hide different parts of the DOM as necessary.

--- js/views/app.js
+++ js/views/app.js
@@ -13,22 +13,26 @@ app.AppView = Backbone.View.extend({
 		this.$input = this.$('#new-food-item');
 		this.$results = this.$('#search-results');
 		this.$main = this.$('#main');
+		this.$calories = this.$('#total-calories');
 
 		this.listenTo(app.Results, 'add', this.addOneResult);
+		this.listenTo(app.Foods, 'add', this.addOneFood);
 
-		this.listenTo(app.Food, 'add', this.addOne);
-		this.listenTo(app.Food, 'reset', this.addAll);
+		this.listenTo(app.Foods, 'remove', this.recalculateCalories);
 
 		this.listenTo(app.Results, 'all', this.render);
+		this.listenTo(app.Foods, 'all', this.render);
 
-		app.Results.fetch();
+		app.Foods.fetch();
 	},
 
 	render: function() {
-      if ( app.Food.length ) {
+      if ( app.Foods.length > 0 ) {
         this.$main.show();
+        this.$calories.show();
       } else {
         this.$main.hide();
+        this.$calories.hide();
       }
       /*if ( app.Results.length ) {
       	//console.l
@@ -39,8 +43,17 @@ app.AppView = Backbone.View.extend({
     },
 
     addOneResult: function(result) {
+    	//console.log(result);
     	var resultView = new app.ResultView({model: result});
     	$('#search-results').append(resultView.render().el);
+    },
+
+    addOneFood: function(food) {
+    	//console.log(food);
+    	var foodView = new app.FoodView({model: food});
+    	$('#food-list').append(foodView.render().el);
+    	//console.log(app.Foods.calculateCalories());
+    	$('#calorie-count').text(app.Foods.calculateCalories());
     },
 
     /*newResult: function(itemName, calories) {
@@ -60,6 +73,10 @@ app.AppView = Backbone.View.extend({
 
     	var nutritionixURL = 'https://api.nutritionix.com/v1_1/search/' + this.$input.val() + '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=8fee4a77&appKey=2d4567a307d2784c203ac27ccd6f5e5c';
 
+    	var nutritionixTimeoutRequest = setTimeout(function() {
+    		$('#search-results').text('Failed to find nutrition resources');
+    	}, 4000);
+
     	$.ajax(nutritionixURL, {
     		dataType: 'json',
     	}).done(function(data) {
@@ -67,10 +84,16 @@ app.AppView = Backbone.View.extend({
     		for (var i = 0; i < data.hits.length; i++) {
     			app.Results.create({name: foodItems[i].fields.item_name, calorieCount: foodItems[i].fields.nf_calories});
     		}
+
+    		clearTimeout(nutritionixTimeoutRequest);
     	});
 
     	this.$input.val('');
 
+    },
+
+    recalculateCalories: function() {
+    	$('#calorie-count').text(app.Foods.calculateCalories());
     }
 
 });
